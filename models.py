6f5064769917428d8d6f0f309701fb1f1ec70a1c
Anh Trinh <anh.trinhtrung@gmail.com>
2018-05-05 22:28:16 +1200

    MODEL9 results
    Changes:
    
    - Add 4th convolution layer
    - Add BatchNormalization2D after each convolution layer
    - Add BatchNormalization1D after each dense layer
    - Using 1000 units for dense layer
    - Training in 2000 epochs

--- models.py
+++ models.py
@@ -21,68 +21,34 @@ class Net(nn.Module):
         # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
         # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
 
-        # MODEL 3
-        # self.conv1 = nn.Conv2d(1, 256, 5) # (220,220)
-        # self.pool1 = nn.MaxPool2d(2) # (110, 110)
-        # self.conv2 = nn.Conv2d(256, 128, 5) # (106, 106)
-        # self.pool2 = nn.MaxPool2d(2) # (53, 53)
-        # self.conv3 = nn.Conv2d(128, 64, 4) # (50, 50)
-        # self.pool3 = nn.MaxPool2d(2) # (25, 25)
-        # self.fc1 = nn.Linear(64 * 25 * 25, 1000)
-        # self.fc1_drop = nn.Dropout(0.5)
-        # self.fc2 = nn.Linear(1000, 500)
-        # self.fc2_drop = nn.Dropout(0.3)
-        # self.fc3 = nn.Linear(500, 136)
 
-        # MODEL 4
-        # self.conv1 = nn.Conv2d(1, 32, 5) # (220,220)
-        # self.pool1 = nn.MaxPool2d(2) # (110, 110)
-        # self.conv2 = nn.Conv2d(32, 64, 5) # (106, 106)
-        # self.pool2 = nn.MaxPool2d(2) # (53, 53)
-        # self.conv3 = nn.Conv2d(64, 128, 4) # (50, 50)
-        # self.pool3 = nn.MaxPool2d(2) # (25, 25)
-        # self.conv4 = nn.Conv2d(128, 256, 3) # (23, 23)
-        # self.pool4 = nn.MaxPool2d(2) # (11, 11)
-        # self.fc1 = nn.Linear(256 * 11 * 11, 1000)
-        # self.fc1_drop = nn.Dropout(0.5)
-        # self.fc2 = nn.Linear(1000, 1000)
-        # self.fc2_drop = nn.Dropout(0.5)
-        # self.fc3 = nn.Linear(1000, 136)
-
-        # MODEL 5
-        # self.conv1 = nn.Conv2d(1, 32, 5) # (220,220)
-        # self.pool1 = nn.MaxPool2d(2) # (110, 110)
-        # self.pool1_drop = nn.Dropout(0.1)
-        # self.conv2 = nn.Conv2d(32, 64, 5) # (106, 106)
-        # self.pool2 = nn.MaxPool2d(2) # (53, 53)
-        # self.pool2_drop = nn.Dropout(0.2)
-        # self.conv3 = nn.Conv2d(64, 128, 4) # (50, 50)
-        # self.pool3 = nn.MaxPool2d(2) # (25, 25)
-        # self.pool3_drop = nn.Dropout(0.3)
-        # self.conv4 = nn.Conv2d(128, 256, 3) # (23, 23)
-        # self.pool4 = nn.MaxPool2d(2) # (11, 11)
-        # self.pool4_drop = nn.Dropout(0.4)
-        # self.fc1 = nn.Linear(256 * 11 * 11, 1000)
-        # self.fc1_drop = nn.Dropout(0.5)
-        # self.fc2 = nn.Linear(1000, 1000)
-        # self.fc2_drop = nn.Dropout(0.6)
-        # self.fc3 = nn.Linear(1000, 136)
-
-        # MODEL 7
-        self.conv1 = nn.Conv2d(1, 32, 5) # (220,220)
-        self.pool1 = nn.MaxPool2d(2) # (110, 110)
+        self.conv1 = nn.Conv2d(1, 32, 3) # 224 - 3 + 1 = 222
+        self.conv1_bn = nn.BatchNorm2d(32)
+        self.pool1 = nn.MaxPool2d(2) # (111, 111)
         self.pool1_drop = nn.Dropout(0.1)
-        self.conv2 = nn.Conv2d(32, 64, 5) # (106, 106)
-        self.pool2 = nn.MaxPool2d(2) # (53, 53)
+
+        self.conv2 = nn.Conv2d(32, 64, 3) # 111 - 3 + 1 = 109
+        self.conv2_bn = nn.BatchNorm2d(64)
+        self.pool2 = nn.MaxPool2d(2) # (54, 54)
         self.pool2_drop = nn.Dropout(0.2)
-        self.conv3 = nn.Conv2d(64, 128, 4) # (50, 50)
-        self.pool3 = nn.MaxPool2d(2) # (25, 25)
+
+        self.conv3 = nn.Conv2d(64, 128, 3) # 54 - 3 + 1 = 52
+        self.conv3_bn = nn.BatchNorm2d(128)
+        self.pool3 = nn.MaxPool2d(2) # (26, 26)
         self.pool3_drop = nn.Dropout(0.3)
-        self.fc1 = nn.Linear(128 * 25 * 25, 500)
+
+        self.conv4 = nn.Conv2d(128, 256, 5) # 26 - 5 + 1 = 22
+        self.conv4_bn = nn.BatchNorm2d(256)
+        self.pool4 = nn.MaxPool2d(2) # (11, 11)
+        self.pool4_drop = nn.Dropout(0.3)
+
+        self.fc1 = nn.Linear(256 * 11 * 11, 1000)
+        self.fc1_bn = nn.BatchNorm1d(1000)
         self.fc1_drop = nn.Dropout(0.5)
-        self.fc2 = nn.Linear(500, 500)
+        self.fc2 = nn.Linear(1000, 1000)
+        self.fc2_bn = nn.BatchNorm1d(1000)
         self.fc2_drop = nn.Dropout(0.5)
-        self.fc3 = nn.Linear(500, 136)
+        self.fc3 = nn.Linear(1000, 136)
 
 
         ## Note that among the layers to add, consider including:
@@ -94,19 +60,23 @@ class Net(nn.Module):
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
         ## x = self.pool(F.relu(self.conv1(x)))
 
-        x = self.pool1(F.relu(self.conv1(x)))
+        x = self.pool1(self.conv1_bn(F.relu(self.conv1(x))))
         x = self.pool1_drop(x)
-        x = self.pool2(F.relu(self.conv2(x)))
+        x = self.pool2(self.conv2_bn(F.relu(self.conv2(x))))
         x = self.pool2_drop(x)
-        x = self.pool3(F.relu(self.conv3(x)))
+        x = self.pool3(self.conv3_bn(F.relu(self.conv3(x))))
         x = self.pool3_drop(x)
+        x = self.pool4(self.conv4_bn(F.relu(self.conv4(x))))
+        x = self.pool4_drop(x)
 
         x = x.view(x.size(0), -1)
 
-        x = F.relu(self.fc1(x))
+        x = self.fc1_bn(F.relu(self.fc1(x)))
         x = self.fc1_drop(x)
-        x = F.relu(self.fc2(x))
+
+        x = self.fc2_bn(F.relu(self.fc2(x)))
         x = self.fc2_drop(x)
+
         x = self.fc3(x)
 
         # a modified x, having gone through all the layers of your model, should be returned
